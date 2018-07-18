51361b720d717ccba68c0e38fffd92645946b1c4
Mark Paine <markpaine@gmail.com>
2018-07-18 06:36:33 -0700

    Hack to allow models based on Orstein Uhlenbeck noise
    This hack is necessary because the TensorFlow model differs based
    on the type of noise used. There's no elegant way around it short
    of changing the TensorFlow graph to include all noise models, but
    only running the current model in the session.

--- baselines/ddpg/evaluation.bash
+++ baselines/ddpg/evaluation.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 interrupt=false
 while true; do
     if [[ $interrupt = true ]]; then
@@ -13,7 +11,7 @@ while true; do
         fi
         model=$(echo "$filename" | sed -e 's,\.index,,' | sed -e 's,saved-models/,,')
         echo "Checking ${model}..."
-        [[ -f $filename ]] || {
+        [ -f "$filename" ] || {
             echo "  The TensorFlow Saver() might have beaten us to deleting this one."
             continue
         }
@@ -25,19 +23,38 @@ while true; do
             --nb-eval-steps 1001 \
             --restore-model-name $model \
             --evaluation \
-            --eval-only)
+            --eval-only 2>&1)
+        echo "$out" | grep "Key adaptive_param_noise_actor/LayerNorm/beta not found in checkpoint"
+        if [ $? = 0 ]; then
+            echo "  This one didn't use adaptive parameter noise. Let's try Ornstein Uhlenbeck."
+            out=$(python -m baselines.ddpg.main \
+                --model 3D \
+                --difficulty 0 \
+                --reward-shaping \
+                --relative-x-pos \
+                --nb-eval-steps 1001 \
+                --restore-model-name $model \
+                --noise-type ou_0.2 \
+                --evaluation \
+                --eval-only 2>&1)
+        fi
         echo "$out" | grep "Assign requires shapes of both tensors to match" && {
-            echo "  Moving it to mismatched-models/";
+            echo "  Moving it to mismatched-models/"
             mv saved-models/${model}.* mismatched-models/
             continue
         }
         echo "$out" | grep "eval/return " | grep ' [6-9][0-9][0-9]' && {
-            echo "  Moving it to top-models/";
+            echo "  Moving it to top-models/"
             mv saved-models/${model}.* top-models/
             continue
         }
         echo "$out" | grep "eval/return " | grep ' [1-9][0-9][0-9][0-9]' && {
-            echo "  Moving it to top-models/";
+            echo "  Moving it to top-models/"
+            mv saved-models/${model}.* top-models/
+            continue
+        }
+        echo "$out" | grep "eval/return " | grep ' [1-9].[0-9]e+[0-9][0-9]' && {  # e.g., 1.1e+03
+            echo "  Moving it to top-models/"
             mv saved-models/${model}.* top-models/
             continue
         }
